8c3a275282f549ab706136d4df104e009e87212f
William Wong <compulim@hotmail.com>
2019-02-12 19:06:51 -0800

    Fix Firefox scroll position locking

--- packages/component/src/ScrollToBottom/Composer.js
+++ packages/component/src/ScrollToBottom/Composer.js
@@ -9,7 +9,8 @@ import InternalContext from './InternalContext';
 import SpineTo from '../SpineTo';
 import StateContext from './StateContext';
 
-const MIN_CHECK_INTERVAL = 17;
+const MIN_CHECK_INTERVAL = 17;       // 1 frame
+const SCROLL_DECISION_DECISION = 34; // 2 frames
 
 function setImmediateInterval(fn, ms) {
   fn();
@@ -85,15 +86,29 @@ export default class Composer extends React.Component {
   enableWorker() {
     clearInterval(this._stickyCheckTimeout);
 
+    let stickyButNotAtEndSince = false;
+
     this._stickyCheckTimeout = setImmediateInterval(
       () => {
         const { state } = this;
         const { stateContext: { sticky }, target } = state;
 
-        if (sticky && target) {
-          const { atEnd } = computeViewState(state);
+        if (sticky && target && !computeViewState(state).atEnd) {
+          if (!stickyButNotAtEndSince) {
+            stickyButNotAtEndSince = Date.now();
+          } else if (Date.now() - stickyButNotAtEndSince > SCROLL_DECISION_DECISION) {
+            // Quirks: In Firefox, after user scroll down, Firefox do two things:
+            //         1. Set to a new "scrollTop"
+            //         2. Fire "scroll" event
+            //         For what we observed, #1 is fired about 20ms before #2. There is a chance that this stickyCheckTimeout is being scheduled between 1 and 2.
+            //         That means, if we just look at #1 to decide if we should scroll, we will always scroll, in oppose to the user's intention.
+            // Repro: Open Firefox, set checkInterval to a lower number, and try to scroll by dragging the scroll handler. It will jump back.
 
-          !atEnd && state.functionContext.scrollToEnd();
+            state.functionContext.scrollToEnd();
+            stickyButNotAtEndSince = false;
+          }
+        } else {
+          stickyButNotAtEndSince = false;
         }
       },
       Math.max(MIN_CHECK_INTERVAL, this.props.checkInterval) || MIN_CHECK_INTERVAL
@@ -114,6 +129,8 @@ export default class Composer extends React.Component {
   }
 
   handleScroll({ timeStampLow }) {
+    console.log(`${ Date.now() }: onScroll`);
+
     // Currently, there are no reliable way to check if the "scroll" event is trigger due to
     // user gesture, programmatic scrolling, or Chrome-synthesized "scroll" event to compensate size change.
     // Thus, we use our best-effort to guess if it is triggered by user gesture, and disable sticky if it is heading towards the start direction.
@@ -207,7 +224,7 @@ export default class Composer extends React.Component {
 }
 
 Composer.defaultProps = {
-  checkInterval: 150,
+  checkInterval: 100,
   debounce: 17
 };
 
