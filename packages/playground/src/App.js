4ef01c0a897e61b6c4acb1ba97a3434e449e181f
William Wong <compulim@hotmail.com>
2019-06-19 13:07:17 -0700

    Move to React hooks and add resize buttons

--- packages/playground/src/App.js
+++ packages/playground/src/App.js
@@ -2,7 +2,7 @@ import { css } from 'glamor';
 import classNames from 'classnames';
 import Interval from 'react-interval';
 import loremIpsum from 'lorem-ipsum';
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import ScrollToEnd, { StateContext } from 'react-scroll-to-bottom';
 
 const FADE_IN_ANIMATION = css.keyframes({
@@ -43,6 +43,10 @@ const CONTAINER_CSS = css({
   marginTop: 10
 });
 
+const LARGE_CONTAINER_CSS = css({
+  height: 600
+});
+
 const SCROLL_VIEW_CSS = css({
   backgroundColor: '#EEE'
 });
@@ -60,101 +64,114 @@ const SCROLL_VIEW_PADDING_CSS = css({
   }
 });
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const SMALL_CONTAINER_CSS = css({
+  height: 300
+});
 
-    this.handleAdd1 = this.handleAdd.bind(this, 1);
-    this.handleAdd10 = this.handleAdd.bind(this, 10);
-    this.handleClear = this.handleClear.bind(this);
-    this.handleIntervalCallback = this.handleIntervalCallback.bind(this);
-    this.handleIntervalEnabledChange = this.handleIntervalEnabledChange.bind(this);
+const createParagraphs = count => new Array(count).fill().map(() => loremIpsum({ units: 'paragraph' }));
 
-    this.state = {
-      intervalEnabled: false,
-      paragraphs: []
-    };
-  }
+const App = () => {
+  const [containerSize, setContainerSize] = useState('');
+  const [intervalEnabled, setIntervalEnabled] = useState(false);
+  const [paragraphs, setParagraphs] = useState(createParagraphs(10));
 
-  componentDidMount() {
-    this.handleAdd(10);
-  }
+  const handleAdd = useCallback(count => setParagraphs([...paragraphs, ...createParagraphs(count)]), [paragraphs, setParagraphs]);
+  const handleAdd1 = useCallback(() => handleAdd(1), [handleAdd]);
+  const handleAdd10 = useCallback(() => handleAdd(10), [handleAdd]);
+  const handleClear = useCallback(() => setParagraphs([]), [setParagraphs]);
+  const handleContainerSizeSmall = useCallback(() => setContainerSize('small'), [setContainerSize]);
+  const handleContainerSizeNormal = useCallback(() => setContainerSize(''), [setContainerSize]);
+  const handleContainerSizeLarge = useCallback(() => setContainerSize('large'), [setContainerSize]);
+  const handleIntervalEnabledChange = useCallback(({ target: { checked: intervalEnabled } }) => setIntervalEnabled(intervalEnabled), []);
+  const containerClassName = useMemo(() => classNames(
+    CONTAINER_CSS + '',
+    containerSize === 'small' ?
+      SMALL_CONTAINER_CSS + ''
+    : containerSize === 'large' ?
+      LARGE_CONTAINER_CSS + ''
+    :
+      ''
+  ), [containerSize]);
 
-  handleAdd(count) {
-    this.setState(state => ({
-      paragraphs: [
-        ...state.paragraphs,
-        ...new Array(count).fill().map(() => loremIpsum({ units: 'paragraph' }))
-      ]
-    }));
-  }
+  const handleKeyDown = useCallback(({ keyCode }) => {
+    switch (keyCode) {
+      case 49: return handleAdd1();
+      case 50: return handleAdd10();
+      case 51: return handleClear();
+      case 52: return handleContainerSizeSmall();
+      case 53: return handleContainerSizeNormal();
+      case 54: return handleContainerSizeLarge();
+      default: break;
+    }
+  }, []);
 
-  handleClear() {
-    this.setState(() => ({ paragraphs: [] }));
-  }
+  useMemo(() => {
+    window.addEventListener('keydown', handleKeyDown);
 
-  handleIntervalCallback() {
-    this.handleAdd(1);
-  }
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
 
-  handleIntervalEnabledChange({ target: { checked: intervalEnabled } }) {
-    this.setState(() => ({ intervalEnabled }));
-  }
-
-  render() {
-    return (
-      <div className={ ROOT_CSS + '' }>
-        <ul className="button-bar">
-          <li>
-            <button onClick={ this.handleAdd1 }>Add new paragraph</button>
-          </li>
-          <li>
-            <button onClick={ this.handleAdd10 }>Add 10 new paragraphs</button>
-          </li>
-          <li>
-            <button onClick={ this.handleClear }>Clear</button>
-          </li>
-          <li>
-            <label>
-              <input
-                checked={ this.state.intervalEnabled }
-                onChange={ this.handleIntervalEnabledChange }
-                type="checkbox"
-              />
-              Add one every second
-            </label>
-          </li>
-        </ul>
-        <div className="panes">
-          <ScrollToEnd className={ CONTAINER_CSS } scrollViewClassName={ SCROLL_VIEW_CSS }>
-            <StateContext.Consumer>
-              { ({ sticky }) =>
-                <div className={ classNames(SCROLL_VIEW_PADDING_CSS + '', { sticky }) }>
-                  { this.state.paragraphs.map(paragraph => <p key={ paragraph }>{ paragraph }</p>) }
-                </div>
-              }
-            </StateContext.Consumer>
-          </ScrollToEnd>
-          <ScrollToEnd className={ CONTAINER_CSS } mode="top">
-            <StateContext.Consumer>
-              { ({ sticky }) =>
-                <div className={ classNames(SCROLL_VIEW_PADDING_CSS + '', { sticky }) }>
-                  { [...this.state.paragraphs].reverse().map(paragraph => <p key={ paragraph }>{ paragraph }</p>) }
-                </div>
-              }
-            </StateContext.Consumer>
-          </ScrollToEnd>
-        </div>
-        { this.state.intervalEnabled &&
-          <Interval
-            callback={ this.handleIntervalCallback }
-            enabled={ true }
-            timeout={ 1000 }
-          />
-        }
+  return (
+    <div className={ ROOT_CSS + '' }>
+      <ul className="button-bar">
+        <li>
+          <button onClick={ handleAdd1 }>Add new paragraph</button>
+        </li>
+        <li>
+          <button onClick={ handleAdd10 }>Add 10 new paragraphs</button>
+        </li>
+        <li>
+          <button onClick={ handleClear }>Clear</button>
+        </li>
+        <li>
+          <button onClick={ handleContainerSizeSmall }>Small</button>
+        </li>
+        <li>
+          <button onClick={ handleContainerSizeNormal }>Normal</button>
+        </li>
+        <li>
+          <button onClick={ handleContainerSizeLarge }>Large</button>
+        </li>
+        <li>
+          <label>
+            <input
+              checked={ intervalEnabled }
+              onChange={ handleIntervalEnabledChange }
+              type="checkbox"
+            />
+            Add one every second
+          </label>
+        </li>
+      </ul>
+      <div className="panes">
+        <ScrollToEnd className={ containerClassName } scrollViewClassName={ SCROLL_VIEW_CSS }>
+          <StateContext.Consumer>
+            { ({ sticky }) =>
+              <div className={ classNames(SCROLL_VIEW_PADDING_CSS + '', { sticky }) }>
+                { paragraphs.map(paragraph => <p key={ paragraph }>{ paragraph }</p>) }
+              </div>
+            }
+          </StateContext.Consumer>
+        </ScrollToEnd>
+        <ScrollToEnd className={ containerClassName } mode="top">
+          <StateContext.Consumer>
+            { ({ sticky }) =>
+              <div className={ classNames(SCROLL_VIEW_PADDING_CSS + '', { sticky }) }>
+                { [...paragraphs].reverse().map(paragraph => <p key={ paragraph }>{ paragraph }</p>) }
+              </div>
+            }
+          </StateContext.Consumer>
+        </ScrollToEnd>
       </div>
-    );
-  }
-}
+      { intervalEnabled &&
+        <Interval
+          callback={ handleAdd1 }
+          enabled={ true }
+          timeout={ 1000 }
+        />
+      }
+    </div>
+  );
+};
 
 export default App;
