9cb07e2b26571b9b3737d944f75a316734bcc292
William Wong <compulim@hotmail.com>
2020-12-31 15:20:02 -0800

    Fix emptying container

--- packages/component/src/ScrollToBottom/Composer.js
+++ packages/component/src/ScrollToBottom/Composer.js
@@ -107,6 +107,10 @@ const Composer = ({ checkInterval, children, debounce, mode, nonce, scroller }) 
   // Function context
   const scrollTo = useCallback(
     (nextAnimateTo, { behavior } = {}) => {
+      if (typeof nextAnimateTo !== 'number' && nextAnimateTo !== '100%') {
+        return console.warn('react-scroll-to-bottom: Arguments passed to scrollTo() must be either number or "100%".');
+      }
+
       // If it is trying to scroll to a position which is not "atEnd", it should set sticky to false after scroll ended.
 
       debug(
@@ -373,43 +377,50 @@ const Composer = ({ checkInterval, children, debounce, mode, nonce, scroller }) 
   );
 
   useEffect(() => {
-    if (sticky) {
+    if (target) {
       let stickyButNotAtEndSince = false;
 
       const timeout = setImmediateInterval(() => {
-        if (sticky && target && !computeViewState({ mode, target }).atEnd) {
-          if (!stickyButNotAtEndSince) {
-            stickyButNotAtEndSince = Date.now();
-          } else if (Date.now() - stickyButNotAtEndSince > SCROLL_DECISION_DURATION) {
-            // Quirks: In Firefox, after user scroll down, Firefox do two things:
-            //         1. Set to a new "scrollTop"
-            //         2. Fire "scroll" event
-            //         For what we observed, #1 is fired about 20ms before #2. There is a chance that this stickyCheckTimeout is being scheduled between 1 and 2.
-            //         That means, if we just look at #1 to decide if we should scroll, we will always scroll, in oppose to the user's intention.
-            // Repro: Open Firefox, set checkInterval to a lower number, and try to scroll by dragging the scroll handler. It will jump back.
+        if (sticky) {
+          if (!computeViewState({ mode, target }).atEnd) {
+            if (!stickyButNotAtEndSince) {
+              stickyButNotAtEndSince = Date.now();
+            } else if (Date.now() - stickyButNotAtEndSince > SCROLL_DECISION_DURATION) {
+              // Quirks: In Firefox, after user scroll down, Firefox do two things:
+              //         1. Set to a new "scrollTop"
+              //         2. Fire "scroll" event
+              //         For what we observed, #1 is fired about 20ms before #2. There is a chance that this stickyCheckTimeout is being scheduled between 1 and 2.
+              //         That means, if we just look at #1 to decide if we should scroll, we will always scroll, in oppose to the user's intention.
+              // Repro: Open Firefox, set checkInterval to a lower number, and try to scroll by dragging the scroll handler. It will jump back.
 
-            // The "animating" check will make sure stickiness is not lost when elements are adding at a very fast pace.
-            if (!animating) {
-              animateFromRef.current = target.scrollTop;
+              // The "animating" check will make sure stickiness is not lost when elements are adding at a very fast pace.
+              if (!animating) {
+                animateFromRef.current = target.scrollTop;
 
-              debug(
-                `%cInterval check%c: Should sticky but not at end, calling %cscrollToSticky()%c to scroll`,
-                ...styleConsole('navy'),
-                ...styleConsole('orange')
-              );
+                debug(
+                  `%cInterval check%c: Should sticky but not at end, calling %cscrollToSticky()%c to scroll`,
+                  ...styleConsole('navy'),
+                  ...styleConsole('orange')
+                );
 
-              scrollToSticky();
+                scrollToSticky();
+              }
+
+              stickyButNotAtEndSince = false;
             }
+          } else {
             stickyButNotAtEndSince = false;
           }
-        } else {
-          stickyButNotAtEndSince = false;
+        } else if (target.scrollHeight <= target.offsetHeight && !sticky) {
+          // When the container is emptied, we will set sticky back to true.
+
+          setSticky(true);
         }
       }, Math.max(MIN_CHECK_INTERVAL, checkInterval) || MIN_CHECK_INTERVAL);
 
       return () => clearInterval(timeout);
     }
-  }, [animating, checkInterval, mode, scroller, scrollToSticky, sticky, target]);
+  }, [animating, checkInterval, mode, scroller, scrollToSticky, setSticky, sticky, target]);
 
   const styleToClassName = useMemo(() => {
     const emotion =
