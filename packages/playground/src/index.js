a7413d1e612e1592ef32f826114c4ccee435c719
William Wong <compulim@hotmail.com>
2021-05-15 21:13:48 -0700

    Fix ESLint issues

--- packages/playground/src/index.js
+++ packages/playground/src/index.js
@@ -1,9 +1,10 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App nonce="a1b2c3d" />, document.getElementById('root'));
